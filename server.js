d56883b870c3a05d64dd4088ca9af30fe929fd56
Alvin Galit <aroblesgalit@gmail.com>
2022-02-16 19:42:18 -0700

    Created example of middleware and another get route

--- server.js
+++ server.js
@@ -4,7 +4,19 @@ const app = express()
 
 const PORT = process.env.PORT || 3000
 
+// Middlewares
+app.use('/posts', () => {
+  console.log('This is a middleware running')
+})
+
 // Routes
+app.get('/', (req, res) => {
+  res.send('We are on home')
+})
+
+app.get('/posts', (req, res) => {
+  res.send('We are on posts')
+})
 
 // Start API server
 app.listen(PORT, function () {
