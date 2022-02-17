49a79a753be4fe741e517d9eaf572b89b72912fb
Alvin Galit <aroblesgalit@gmail.com>
2022-02-16 19:53:13 -0700

    Installed mongoose and created connection to DB

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const express = require('express')
-
 const app = express()
+const mongoose = require('mongoose')
+
+require('dotenv').config()
 
 const PORT = process.env.PORT || 3000
-
-// Middlewares
-app.use('/posts', () => {
-  console.log('This is a middleware running')
-})
 
 // Routes
 app.get('/', (req, res) => {
@@ -17,6 +14,18 @@ app.get('/', (req, res) => {
 app.get('/posts', (req, res) => {
   res.send('We are on posts')
 })
+
+// Connect to DB
+mongoose.connect(
+  process.env.MONGODB_URI,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  () => {
+    console.log('Connected to DB!')
+  }
+)
 
 // Start API server
 app.listen(PORT, function () {
