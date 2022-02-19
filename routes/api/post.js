e24ce72d652a290c3ee485a9f27de017c634c9d9
Alvin Galit <aroblesgalit@gmail.com>
2022-02-18 22:55:30 -0700

    Created an api route for a post request in Post

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,8 +1,13 @@
 const router = require('express').Router()
 
 // Matches with '/api/posts'
-router.route('/').get((req, res) => {
-  res.send('We are on posts')
-})
+router
+  .route('/')
+  .get((req, res) => {
+    res.send('We are on posts')
+  })
+  .post((req, res) => {
+    console.log(req.body)
+  })
 
 module.exports = router
