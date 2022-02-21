4aa5022a7c46fd317ecb2b8b911cd2be1bc6ecdf
Alvin Galit <aroblesgalit@gmail.com>
2022-02-20 17:09:58 -0700

    Created users controller with a method for registering a user

---
+++ conrollers/usersController.js
@@ -0,0 +1,22 @@
+const db = require('../models')
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+
+module.exports = {
+  registerUser: async function (req, res) {
+    try {
+      const salt = await bcrypt.genSalt(saltRounds)
+      const hash = await bcrypt.hash(req.body.password, salt)
+
+      const newUser = await db.User.create({
+        username: req.body.username,
+        password: hash
+      })
+
+      // Redirect to login page
+      res.redirect(307, '/')
+    } catch (err) {
+      res.status(401).json(err)
+    }
+  }
+}
