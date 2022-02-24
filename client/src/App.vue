96ff795a97b4e8d16bd60dd12e5a2ed344bdfc1c
Alvin Galit <aroblesgalit@gmail.com>
2022-02-23 22:46:49 -0700

    Grabbed form template from getbootstrap

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,48 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <main class="form-signin">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+      <input type="text" class="form-control" placeholder="username" required />
+      <input
+        type="password"
+        class="form-control"
+        placeholder="password"
+        required
+      />
+
+      <button class="w-100 btn btn-lg btn-primary" type="submit">
+        Sign in
+      </button>
+    </form>
+  </main>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
 }
 
-#nav {
-  padding: 30px;
+.form-signin .checkbox {
+  font-weight: 400;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+.form-signin .form-floating:focus-within {
+  z-index: 2;
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+.form-signin input[type='email'] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+
+.form-signin input[type='password'] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
 }
 </style>
