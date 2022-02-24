740c64a12703d6e3a36a41383db3c37bbd2b6e4a
Alvin Galit <aroblesgalit@gmail.com>
2022-02-24 16:04:13 -0700

    Created vuex action for registering a user

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -2,11 +2,22 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    user: {}
   },
   mutations: {
+    newUser: (state, user) => (state.user = user)
   },
   actions: {
+    async registerUser ({ commit }, data) {
+      const response = await axios.post(
+        'http://localhost:8080/api/user/register',
+        data
+      )
+
+      commit('newUser', response.data)
+    }
   },
-  modules: {
+  getters: {
+    getUser: state => state.user
   }
 })
