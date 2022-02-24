756c297c57e595bd52475230227af5131949f1b1
Alvin Galit <aroblesgalit@gmail.com>
2022-02-23 22:56:38 -0700

    Created Login view for the login form

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,26 +2,7 @@
   <div>
     <Nav />
     <main class="form-signin">
-      <form>
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-
-        <input
-          type="text"
-          class="form-control"
-          placeholder="username"
-          required
-        />
-        <input
-          type="password"
-          class="form-control"
-          placeholder="password"
-          required
-        />
-
-        <button class="w-100 btn btn-lg btn-primary" type="submit">
-          Sign in
-        </button>
-      </form>
+      <router-view />
     </main>
   </div>
 </template>
---
+++ client/src/views/Login.vue
@@ -0,0 +1,25 @@
+<template>
+  <form>
+    <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+
+    <input type="text" class="form-control" placeholder="username" required />
+    <input
+      type="password"
+      class="form-control"
+      placeholder="password"
+      required
+    />
+
+    <button class="w-100 btn btn-lg btn-primary" type="submit">
+      Sign in
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style></style>
